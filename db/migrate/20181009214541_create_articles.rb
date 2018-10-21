61991aba2883d4cab0b16aaabe9f81e7585eb7de
Pablo Estrada <pablof300@gmail.com>
2018-10-21 18:33:15 -0400

    Modified the migration file

--- db/migrate/20181009214541_create_articles.rb
+++ db/migrate/20181009214541_create_articles.rb
@@ -5,10 +5,6 @@ class CreateArticles < ActiveRecord::Migration[5.2]
       t.text :description
       t.datetime :created_at
       t.datetime :updated_at
-
-      validates :title, presence: true, length: {minimum: 3, maximum: 50}
-      validates :description, presence:true, length: {minimum: 3, maximum: 50}
-
     end
   end
 end
