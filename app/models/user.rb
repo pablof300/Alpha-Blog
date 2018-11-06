eea790e6cc942d8a75928ba2663d63983ef0c285
Pablo Estrada <pablof300@gmail.com>
2018-11-06 15:13:59 -0500

    Added user validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
 
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username,
+  presence: true,
+  uniqueness: { case_sensitive: false},
+  length: { minimum: 3, maximum: 25}
+
+  validates :email,
+  presence: true,
+  format: { with: EMAIL_REGEX, message: "Your email is invalid" },
+  length: { minimum: 3, maximum: 50}
 end
