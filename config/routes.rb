c46a0a67450eda2d109fababbc5fc9c2172c451c
Pablo Estrada <pablof300@gmail.com>
2018-11-08 14:15:29 -0500

    Added user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "pages#home"
-  
+
   get "about", to: "pages#about"
   get "portfolio/pablo", to: "portfolio#pablo"
 
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
+
   resources :articles
+  resources :users, expect:[:new]
 end
