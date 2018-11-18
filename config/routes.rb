db371b96df4b6f105f923ae68240e4dcd13e5b31
Pablo Estrada <pablof300@gmail.com>
2018-11-17 21:03:40 -0500

    Added login/sign out enforcement to the app

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get "signup", to: "users#new"
   post "users", to: "users#create"
 
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
   resources :articles
   resources :users, except:[:new]
 end
